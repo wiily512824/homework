444770c094fb9ae29d07682baddfd6a377cb45ef
wiily512824 <62005832+wiily512824@users.noreply.github.com>
2020-04-07 00:08:41 +0900

    실패했습니다
    DB에 저장이 안되고 주문하기 버튼에서 다시 리로드 시키는것도 잘 모르겠습니다.

---
+++ python/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/order', methods=['POST'])
+def write_order():
+    name_receive = request.form['name_give']
+    option_receive = request.form['option_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+
+    order = {
+        'name': name_receive,
+        'option': option_receive,
+        'address': address_receive,
+        'phone': phone_receive
+    }
+
+    db.orders.insert_one(order)
+    return jsonify({'result': 'success', 'msg': '주문이 성공적으로 접수되었습니다.'})
+
+
+@app.route('/order', methods=['GET'])
+def read_orders():
+    orders = list(db.orders.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'orders': orders})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
